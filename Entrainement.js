e2b2f2260f0e41cccb4ac5bfa461b02b343d12a9
Armand Bris <58659794+arbris@users.noreply.github.com>
2020-04-14 16:31:11 +0200

    Update Entrainement.js
    Exercice Couleur

--- Entrainement.js
+++ Entrainement.js
@@ -1,16 +1,4 @@
-let A;
-console.log(A);
-let A = 42; //pour un int (entier)
-let A = 4.2; //pour float (nombreà virgule)
-let A = transformIntoArray("Hello World") //pour récupérer un tableau de strings (tableau de phrases)
-6
-4 + 2
-42
-let age;
-4 = "mineur"
-18 = "majeur"
-
-let couleur = "bleu" = A;
+let couleur = "bleu";
 if (couleur = "bleu") {
     console.log("La couleur est bleu");
 } else if (couleur = "rouge") {
