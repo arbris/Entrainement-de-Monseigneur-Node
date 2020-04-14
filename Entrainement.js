b646b0d0e427466d8ab8deb681e6cbc70186c743
Armand Bris <58659794+arbris@users.noreply.github.com>
2020-04-14 15:39:13 +0200

    Update Entrainement.js
    Exercice Âge

--- Entrainement.js
+++ Entrainement.js
@@ -1,2 +1,11 @@
 let A;
 console.log(A);
+let A = 42; //pour un int (entier)
+let A = 4.2; //pour float (nombreà virgule)
+let A = transformIntoArray("Hello World") //pour récupérer un tableau de strings (tableau de phrases)
+6
+4 + 2
+42
+let age;
+4 = "mineur"
+18 = "majeur"
